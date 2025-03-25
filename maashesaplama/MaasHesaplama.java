0b2fcaf9c22df4dc0bdcea7e9e4349c304d8f8ea
zero00d <119650796+zero00d@users.noreply.github.com>
2025-03-25 17:14:04 +0300

    Add files via upload

---
+++ maashesaplama/MaasHesaplama.java
@@ -0,0 +1,14 @@
+
+package maashesaplama;
+
+
+public class MaasHesaplama {
+
+    
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Kemal",2000,45,1985);
+        
+        System.out.println(e1.toString());
+    }
+    
+}
